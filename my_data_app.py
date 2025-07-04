f63022e8985d682b7588aea4c6b1a4ba5849acc2
Kadiatou Kanté <111257428+KanteKadiatou@users.noreply.github.com>
2025-07-04 20:15:03 +0000

    Update my_data_app.py

--- my_data_app.py
+++ my_data_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 
-st.markdown("<h1 style='text-align: center; color: black;'>KADI DATA APP</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: black;'>KADI DATA APP ;)</h1>", unsafe_allow_html=True)
 
 st.markdown("""
 This app allows you to download scraped data on motocycles from expat-dakar 
